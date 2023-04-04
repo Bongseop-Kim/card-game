2d64398fdf1f6c29a770bfbcd8e3bf1cea091c75
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-04-04 17:30:54 +0900

    Home 페이지 생성

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { MonsterDictionary } from "./pages/MonsterDictionay.jsx";
 import { ChannelSelection } from "./pages/ChannelSelection.jsx";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/monsterDictionary" element={<MonsterDictionary />} />
           <Route path="/channelSelection" element={<ChannelSelection />} />
           <Route path="/login" element={<Login />} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { Layout } from "../components/Layout";
+import { ReactComponent as Lock } from "../assets/lock.svg";
+import { ReactComponent as UnLock } from "../assets/unlock.svg";
+const rooms = [
+  { id: 1, title: "~~의 방", lock: true, cntMember: 1, isStart: false },
+  { id: 1, title: "~~의 방", lock: false, cntMember: 1, isStart: true },
+  { id: 1, title: "~~의 방", lock: false, cntMember: 1, isStart: false },
+  { id: 1, title: "~~의 방", lock: false, cntMember: 1, isStart: false },
+  { id: 1, title: "~~의 방", lock: false, cntMember: 1, isStart: false },
+];
+export const Home = () => {
+  return (
+    <Layout>
+      <section className="grid grid-cols-2 gap-4 overflow-y-scroll">
+        {rooms.map((room) => {
+          return (
+            <div
+              className={`flex border rounded-lg p-4 text-xl items-center ${
+                room.isStart ? "bg-gray-400" : "hover:cursor-pointer"
+              }`}
+            >
+              <div>{room.id}</div>
+              <div className="border-l p-2 m-2 w-full">{room.title}</div>
+              <div className="flex flex-col items-center">
+                <div className="text-sm">{room.cntMember} / 2</div>
+                <div>
+                  {room.lock ? <Lock className="fill-red-500 w-8" /> : <UnLock className="fill-blue-500 w-8" />}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </Layout>
+  );
+};
